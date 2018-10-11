58b9975674844f0790a2f50c24cda0dfddfa3135
Maksim Zakharenka <Maksim_Zakharenka@epam.com>
2018-10-11 18:30:20 +0300

    facebook login : inprogress

--- MindGames/app/src/main/java/com/example/mvmax/mindgames/activity/AuthActivity.java
+++ MindGames/app/src/main/java/com/example/mvmax/mindgames/activity/AuthActivity.java
@@ -7,14 +7,57 @@ import android.widget.Toast;
 
 import com.example.mvmax.mindgames.R;
 import com.example.mvmax.mindgames.activity.base.BaseActivity;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 
+import java.util.Arrays;
+
 public class AuthActivity extends BaseActivity {
+
+    private static final String EMAIL = "email";
+
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+
+        loginButton = findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList(EMAIL));
+
+        callbackManager = CallbackManager.Factory.create();
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(final LoginResult loginResult) {
+                // App code
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(final FacebookException exception) {
+                // App code
+            }
+        });
+
+//        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+//        boolean isLoggedIn = accessToken != null && !accessToken.isExpired();
+    }
+
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     public void onSkipAuth(final View view) {
